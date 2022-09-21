6ca8409ed79cc768e1235744a04b60695ed25635
Caio Giffoni <caio_cgf@hotmail.com>
2022-09-21 09:49:31 -0300

    :sparkles: create: dockerfile n docker compose

--- clients-archive-node-js/src/data-source.ts
+++ clients-archive-node-js/src/data-source.ts
@@ -6,7 +6,7 @@ export const AppDataSource = new DataSource({
   host: "localhost",
   port: 5432,
   username: process.env.POSTGRES_USER,
-  password: process.env.POSTGRES_PWD,
+  password: process.env.POSTGRES_PASSWORD,
   database: process.env.POSTGRES_DB,
   synchronize: false,
   logging: true,
