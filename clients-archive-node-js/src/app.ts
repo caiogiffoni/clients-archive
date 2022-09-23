2a144e73eabc8f7874e6c2b130bf03f1b1b52f40
Caio Giffoni <caio_cgf@hotmail.com>
2022-09-23 20:06:04 -0300

    :sparkles: feature: added cors

--- clients-archive-node-js/src/app.ts
+++ clients-archive-node-js/src/app.ts
@@ -2,9 +2,12 @@ import express from "express";
 import "express-async-errors"; // tem que ficar acima do approutes
 import { appRoutes } from "./routes";
 import { errorMiddleware } from "./middlewares/error.middleware";
-import { Request, Response } from "express";
+
+var cors = require("cors");
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 
