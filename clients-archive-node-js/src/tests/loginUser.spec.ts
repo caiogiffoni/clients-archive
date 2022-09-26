ba24db8f95e8bcc2b0006e6b2b42c546fca77eeb
Caio Giffoni <caio_cgf@hotmail.com>
2022-09-26 20:27:44 -0300

    feature: login test

---
+++ clients-archive-node-js/src/tests/loginUser.spec.ts
@@ -0,0 +1,62 @@
+import { DataSource } from "typeorm";
+import { describe, expect, test, beforeAll, afterAll } from "@jest/globals";
+import app from "../../src/app";
+import request from "supertest";
+import { AppDataSource } from "../data-source";
+
+describe("Testing POST /login", () => {
+  let connection: DataSource;
+
+  interface User {
+    name: string;
+    email: string;
+    password?: string;
+  }
+
+  interface UserLogin {
+    email: string;
+    password?: string;
+  }
+
+  let testUser: User = {
+    name: "miguel",
+    email: "testemiguel@hotmail.com",
+    password: "123456Ab!",
+  };
+
+  let loginUser: UserLogin = {
+    email: "testemiguel@hotmail.com",
+    password: "123456Ab!",
+  };
+
+  beforeAll(async () => {
+    await AppDataSource.initialize()
+      .then((res) => (connection = res))
+      .catch((err) => {
+        console.error("Error during Data Source initialization", err);
+      });
+
+    await request(app).post("/users").send(testUser);
+  });
+
+  afterAll(async () => {
+    await connection.destroy();
+  });
+
+  test("Testing POST /login route", async () => {
+    const response = await request(app).post("/login").send(loginUser);
+
+    expect(response.status).toBe(201);
+    expect(response.body).toHaveProperty("token");
+    expect(typeof response.body.token).toBe("string");
+  });
+
+  test("Testing invalid login", async () => {
+    loginUser.password = "123";
+    const response = await request(app).post("/login").send(loginUser);
+
+    expect(response.status).toBe(403);
+    expect(response.body).toHaveProperty("message", "Wrong email/password");
+    loginUser.password = "123456";
+  });
+});
